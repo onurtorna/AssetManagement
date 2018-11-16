9bd46d97864cf917d7d4150419accd9a4de1bd9e
Onur Torna <onurtorna@gmail.com>
2018-11-16 23:04:43 +0300

    Create project

---
+++ AssetManagement/AssetManagement/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AssetManagement
+//
+//  Created by Onur Torna on 16.11.2018.
+//  Copyright © 2018 Onur Torna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
