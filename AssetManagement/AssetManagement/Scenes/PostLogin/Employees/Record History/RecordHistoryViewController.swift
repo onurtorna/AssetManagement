a0f36dc8c91d72cb065dc8878adbf78a3f98ba24
Onur Torna <onurtorna@gmail.com>
2018-11-18 22:10:27 +0300

    Add record history view controller

---
+++ AssetManagement/AssetManagement/Scenes/PostLogin/Employees/Record History/RecordHistoryViewController.swift
@@ -0,0 +1,27 @@
+//
+//  RecordHistoryViewController.swift
+//  AssetManagement
+//
+//  Created by Onur Torna on 18.11.2018.
+//  Copyright © 2018 Onur Torna. All rights reserved.
+//
+
+import UIKit
+
+final class RecordHistoryViewController: UIViewController {
+
+    private enum Constant {
+        static let cellReuseIdentifier = "RecordListTableViewCell"
+    }
+
+    @IBOutlet private weak var tableView: UITableView!
+
+}
+
+// MARK: - StoryboardLoadable
+extension RecordHistoryViewController: StoryboardLoadable {
+
+    static var defaultStoryboardName: String {
+        return Global.Storyboard.employees
+    }
+}
